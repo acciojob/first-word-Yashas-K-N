3607f52b11e7004e5b5be1def9aa784dea8196fb
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2025-01-19 23:30:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function r(s) {
     
 		
 	for(let i=0; i<s.length; i++){
-		 if( s[i] !== ' '){
+		 if(s[i] !== ' '){
 		  result += s[i];
 		 }
 		else{
@@ -16,7 +16,7 @@ function r(s) {
 	
   return result;
 }
-let res = r(s);
+let res = r("rkskk ");
 console.log(res);
 
 // Do not change the code below
